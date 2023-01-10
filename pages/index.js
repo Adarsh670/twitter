5db6ade910068115abcf101d0a78721cc0685077
adarsh <adarsh.kasap@gmail.com>
2023-01-10 17:39:15 +0530

    create feed component and its header

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-import Sidebar from '../components/Sidebar'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import Feed from "../components/Feed";
+import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
@@ -15,9 +11,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-     <main className='flex min-h-screen max-w-7xl mx-auto'>
-      <Sidebar/>
-     </main>
+      <main className="flex min-h-screen max-w-7xl mx-auto">
+        <Sidebar />
+        <Feed />
+      </main>
     </>
-  )
+  );
 }
